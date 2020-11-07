c99240d92c120bcd05e75eefc76d6a481d9466d8
Eric Widjaja <ericwidjaja@pursuit.org>
2020-11-06 22:31:40 -0500

    create new project-ContactsAppTwo

---
+++ ContactsAppTwo/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ContactsAppTwo
+//
+//  Created by Eric Widjaja on 11/6/20.
+//  Copyright © 2020 ericSwidjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ContactsAppTwo/Model/Contact.swift
@@ -0,0 +1,9 @@
+//
+//  Contact.swift
+//  ContactsAppTwo
+//
+//  Created by Eric Widjaja on 11/6/20.
+//  Copyright © 2020 ericSwidjaja. All rights reserved.
+//
+
+import Foundation
